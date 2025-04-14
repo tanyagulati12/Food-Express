b507f6846360c381a9262b3608be6625bfff76bc
tanyagulati12 <122425197+tanyagulati12@users.noreply.github.com>
2025-04-14 17:40:00 +0530

    Update App.jsx changed backend url

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  const url = "https://food-express-ek4e.onrender.com"
 
   return (
     <div>
